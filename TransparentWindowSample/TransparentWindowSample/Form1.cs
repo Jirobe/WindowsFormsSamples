f376955660628b460d80fc87dcc6fe7f65643895
Jirobe <38680274+Jirobe@users.noreply.github.com>
2020-12-19 23:43:00 +0900

    Windows Forms　ウィンドウの背景が透明なもののサンプル
    Windows Forms
    ウィンドウの背景が透明なもののサンプル

---
+++ TransparentWindowSample/TransparentWindowSample/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace TransparentWindowSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //ウィンドウを透明に Start
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            //ここの色は何でもよい
+            TransparencyKey = Color.Red;
+            e.Graphics.FillRectangle(new SolidBrush(TransparencyKey), ClientRectangle);
+
+            base.OnPaint(e);
+        }
+        //ウィンドウを透明に End
+    }
+}
